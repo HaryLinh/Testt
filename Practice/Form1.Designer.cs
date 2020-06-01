69ab19fcabc1ee3d9483f06dfab917a600c98df1
HaryLinh <Anhlinh12369@icloud.com>
2020-06-01 20:15:53 +0700

    Chap1

---
+++ Practice/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MaVT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenVT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DvTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaLoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtTenHang = new System.Windows.Forms.TextBox();
+            this.cbbLoaiHang = new System.Windows.Forms.ComboBox();
+            this.cbbDonViTinh = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMaVT = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaVT,
+            this.TenVT,
+            this.DvTinh,
+            this.MaLoai});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(758, 242);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // MaVT
+            // 
+            this.MaVT.DataPropertyName = "MaVT";
+            this.MaVT.HeaderText = "MaVT";
+            this.MaVT.Name = "MaVT";
+            this.MaVT.Width = 200;
+            // 
+            // TenVT
+            // 
+            this.TenVT.DataPropertyName = "TenVT";
+            this.TenVT.HeaderText = "TenVT";
+            this.TenVT.Name = "TenVT";
+            this.TenVT.Width = 200;
+            // 
+            // DvTinh
+            // 
+            this.DvTinh.DataPropertyName = "DvTinh";
+            this.DvTinh.HeaderText = "DvTinh";
+            this.DvTinh.Name = "DvTinh";
+            this.DvTinh.Width = 120;
+            // 
+            // MaLoai
+            // 
+            this.MaLoai.DataPropertyName = "MaLoai";
+            this.MaLoai.HeaderText = "MaLoai";
+            this.MaLoai.Name = "MaLoai";
+            this.MaLoai.Width = 200;
+            // 
+            // txtTenHang
+            // 
+            this.txtTenHang.Location = new System.Drawing.Point(125, 52);
+            this.txtTenHang.Name = "txtTenHang";
+            this.txtTenHang.Size = new System.Drawing.Size(100, 20);
+            this.txtTenHang.TabIndex = 2;
+            // 
+            // cbbLoaiHang
+            // 
+            this.cbbLoaiHang.FormattingEnabled = true;
+            this.cbbLoaiHang.Items.AddRange(new object[] {
+            "USD",
+            "Dong",
+            "Yen"});
+            this.cbbLoaiHang.Location = new System.Drawing.Point(125, 115);
+            this.cbbLoaiHang.Name = "cbbLoaiHang";
+            this.cbbLoaiHang.Size = new System.Drawing.Size(121, 21);
+            this.cbbLoaiHang.TabIndex = 3;
+            // 
+            // cbbDonViTinh
+            // 
+            this.cbbDonViTinh.FormattingEnabled = true;
+            this.cbbDonViTinh.Items.AddRange(new object[] {
+            "USD",
+            "Dong"});
+            this.cbbDonViTinh.Location = new System.Drawing.Point(388, 59);
+            this.cbbDonViTinh.Name = "cbbDonViTinh";
+            this.cbbDonViTinh.Size = new System.Drawing.Size(121, 21);
+            this.cbbDonViTinh.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(296, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Don vi tinh";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Don vi tinh";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "TenHang";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Them";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "MaVT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblMaVT
+            // 
+            this.lblMaVT.AutoSize = true;
+            this.lblMaVT.Location = new System.Drawing.Point(122, 13);
+            this.lblMaVT.Name = "lblMaVT";
+            this.lblMaVT.Size = new System.Drawing.Size(0, 13);
+            this.lblMaVT.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(540, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(667, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblMaVT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbbDonViTinh);
+            this.Controls.Add(this.cbbLoaiHang);
+            this.Controls.Add(this.txtTenHang);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaVT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenVT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DvTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaLoai;
+        private System.Windows.Forms.TextBox txtTenHang;
+        private System.Windows.Forms.ComboBox cbbLoaiHang;
+        private System.Windows.Forms.ComboBox cbbDonViTinh;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMaVT;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Practice/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadCbbLoaiHang();
+            LoadData();
+
+
+        }
+        
+        private void InsertHang()
+        {
+            string connnect = "Data Source=DESKTOP-IKP0EDP;Initial Catalog=practice;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connnect);
+            connection.Open();
+            string sql = "Insert into VatTu values(@TenHang, @DonViTinh, @MaLoai)";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("TenHang", txtTenHang.Text);
+            cmd.Parameters.AddWithValue("DonViTinh", cbbDonViTinh.Text);
+            cmd.Parameters.AddWithValue("MaLoai", int.Parse( cbbLoaiHang.SelectedValue.ToString()));
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            LoadData();
+        }
+        private void LoadCbbLoaiHang()
+        {
+            string connnect = "Data Source=DESKTOP-IKP0EDP;Initial Catalog=practice;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connnect);
+            connection.Open();
+            string sql = "Select * from LoaiVT";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            SqlDataReader dt = cmd.ExecuteReader();
+            DataTable data = new DataTable();
+            data.Load(dt);
+            connection.Close();
+            
+            cbbLoaiHang.DisplayMember = "TenLoaiVT";
+            cbbLoaiHang.ValueMember = "MaLoai";
+            cbbLoaiHang.DataSource = data;
+        }
+        private void LoadData()
+        {
+            string connnect = "Data Source=DESKTOP-IKP0EDP;Initial Catalog=practice;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connnect);
+            connection.Open();
+            string sql = "Select * from VatTu";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            SqlDataReader dt = cmd.ExecuteReader();
+            DataTable data = new DataTable();
+            data.Load(dt);
+            dataGridView1.DataSource = data;
+            connection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InsertHang();
+        }
+
+     
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtTenHang.Text = row.Cells[1].Value.ToString();
+                cbbLoaiHang.SelectedValue = int.Parse(row.Cells[3].Value.ToString());
+                cbbDonViTinh.Text = row.Cells[2].Value.ToString();
+                lblMaVT.Text = row.Cells[0].Value.ToString();
+            }
+                
+        }
+
+        private void UpdateVT()
+        {
+            string connnect = "Data Source=DESKTOP-IKP0EDP;Initial Catalog=practice;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connnect);
+            connection.Open();
+            string sql = "update VatTu set TenVT = @TenHang, DvTinh =  @DonViTinh, Maloai = @MaLoai where MaVT = @MaVT";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("TenHang", txtTenHang.Text);
+            cmd.Parameters.AddWithValue("DonViTinh", cbbDonViTinh.Text);
+            cmd.Parameters.AddWithValue("MaLoai", int.Parse(cbbLoaiHang.SelectedValue.ToString()));
+            cmd.Parameters.AddWithValue("MaVT", int.Parse(lblMaVT.Text));
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            LoadData();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UpdateVT();
+        }
+
+        private void DeleteVT()
+        {
+            string connnect = "Data Source=DESKTOP-IKP0EDP;Initial Catalog=practice;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connnect);
+            connection.Open();
+            string sql = "delete from VatTu where MaVT = @MaVT";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("MaVT", int.Parse(lblMaVT.Text));
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            LoadData();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DeleteVT();
+        }
+    }
+}
